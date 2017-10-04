19999b1d95e26baae92819352c609cab8feb55b1
PatrickHennis <patrickhennis@gmail.com>
2017-10-03 23:29:59 -0400

    added statement implementation

--- src/statement.cpp
+++ src/statement.cpp
@@ -1,5 +1,163 @@
-//
-// Created by Patrick Hennis on 9/26/17.
-//
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=8 sts=4 et sw=4 tw=80: */
+/*
+ * Copyright (c) 2016.  Jefferson Lab (JLab). All rights reserved. Permission
+ * to use, copy, modify, and distribute  this software and its documentation for
+ * educational, research, and not-for-profit purposes, without fee and without a
+ * signed licensing agreement.
+ *
+ * IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL
+ * INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ * OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS
+ * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ * PROVIDED HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * This software was developed under the United States Government license.
+ * For more information contact author at gurjyan@jlab.org
+ * Department of Experimental Nuclear Physics, Jefferson Lab.
+ */
 
 #include "statement.hpp"
+
+namespace clara {
+namespace statement {
+
+    Statement::Statement(std::string statement_string, std::string service_name) {
+        if (statement_string.find(service_name) != nullptr) {
+            this->statement_string_ = statement_string;
+            this->service_name_ = service_name;
+            process(statement_string);
+        } else {
+            throw std::logic_error{"irrelevant statement"};
+        }
+    }
+
+    std::string Statement::get_service_name() {
+        return service_name_;
+    }
+
+    std::string Statement::get_statement_string() {
+        return statement_string_;
+    }
+
+    std::set<std::string> Statement::get_input_links() {
+        return input_links;
+    }
+
+    std::set<std::string> Statement::get_output_links() {
+        return output_links;
+    }
+
+    std::map<std::string, clara::EngineData> get_log_and_inputs() {
+        return log_and_inputs;
+    };
+
+    void Statement::process(std::string statement) {
+        input_links.clear();
+        output_links.clear();
+
+        if(statement.find(service_name_) != std::string::npos) {
+            parse_linked(service_name_, statement);
+            if (is_log_and(service_name_, statement)) {
+                for (String sn : input_links) {
+                    log_and_inputs.insert(sn, 0);
+                }
+            }
+        }
+    }
+
+    void Statement::parse_linked(std::string service_name, std::string statement) {
+        std::vector<std::string> element_set;
+
+        std::vector<std::string> st = tokenize(statement, "+");
+        for (std::string el : st) {
+            el = remove_first(el, '&');
+            el = remove_first(el, '{');
+            element_set.push_back(el);
+        }
+
+        int index = -1;
+        for (std::string s : element_set) {
+            index++;
+            if (s.find(service_name) != std::string::npos) {
+                break;
+            }
+        }
+        if (index == -1) {
+            throw std::logic_error{"Routing statement parsing exception. "
+                       "Service name can not be found in the statement."};
+        } else {
+            int pIndex = index - 1;
+            if (pIndex >= 0) {
+                std::string element = element_set.at(pIndex);
+
+                input_links = tokenize(element, ",");
+            }
+
+            int nIndex = index + 1;
+            if (element_set.size() > nIndex) {
+                std::string element = element_set.at(nIndex);
+                output_links = tokenize(element, ",");
+            }
+        }
+
+    }
+
+    bool Statement::is_log_and(std::string service_name, std::string composition) {
+        std::string ac = "&" + service_name;
+
+        std::vector<std::string> st = tokenize(composition, "+");
+        for (std::string s : st) {
+            if (s == ac) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    std::string Statement::to_string() {
+        return "Statement {"
+                "serviceName='" + service_name_ + "'"
+                ", statementString='" + statement_string_ + "'"
+                ", logAndInputs='"
+                ", inputLinks=" + input_links + ""
+                ", outputLinks=" + output_links + ""
+                "}";
+    }
+
+    bool Statement::equals(Statement s) {
+        if (!(this->service_name_ == s.service_name_)) {
+            return false;
+        }
+        if (!(this->statement_string_ == s.statement_string_)) {
+            return false;
+        }
+        if (!(this->log_and_inputs == s.log_and_inputs)) {
+            return false;
+        }
+        if (!(this->input_links == s.input_links)) {
+            return false;
+        }
+        if (!(this->output_links == s.output_links)) {
+            return false;
+        }
+        return true;
+    }
+
+    int Statement::hash_code() {
+        int result = std::hash<std::string>{}(service_name_);
+        result = 31 * result + std::hash<std::string>{}(statement_string_);
+        //result = 31 * result + std::hash<std::string>{}(log_and_inputs);
+        //result = 31 * result + std::hash<std::string>{}(input_links);
+        //result = 31 * result + std::hash<std::string>{}(output_links);
+        return result;
+    }
+
+}
+}
--- src/statement.hpp
+++ src/statement.hpp
@@ -1,14 +1,81 @@
-//
-// Created by Patrick Hennis on 9/26/17.
-//
-
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=8 sts=4 et sw=4 tw=80: */
+/*
+ * Copyright (c) 2016.  Jefferson Lab (JLab). All rights reserved. Permission
+ * to use, copy, modify, and distribute  this software and its documentation for
+ * educational, research, and not-for-profit purposes, without fee and without a
+ * signed licensing agreement.
+ *
+ * IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL
+ * INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ * OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS
+ * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ * PROVIDED HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * This software was developed under the United States Government license.
+ * For more information contact author at gurjyan@jlab.org
+ * Department of Experimental Nuclear Physics, Jefferson Lab.
+ */
 #ifndef CLARA_STATEMENT_HPP
 #define CLARA_STATEMENT_HPP
+
+#include "engine_data_helper.hpp"
+#include <set>
+#include <map>
+
+inline std::vector<std::string> tokenize(std::string s, std::string delim)
+{
+    std:: vector<std::string> tv;
+
+    size_t pos = 0;
+    std::string token;
+    while ((pos = s.find(delim)) != std::string::npos) {
+        token = s.substr(0, pos);
+        tv.push_back(token);
+        s.erase(0, pos + delim.length());
+    }
+    return tv;
+}
+
+inline std::string remove_first(std::string str, char d) {
+    auto pos = str.find(d);
+    if (pos != std::string::npos) {
+        str.erase(pos);
+    }
+    return str;
+}
 
 namespace clara {
 namespace statement {
 
 class Statement {
+public:
+    Statement(std::string statement_string, std::string service_name);
+    std::string get_service_name();
+    std::string get_statement_string();
+    std::set<std::string> get_input_links();
+    std::set<std::string> get_output_links();
+    std::map<std::string, EngineData>get_log_and_inputs();
+    std::string to_string();
+    bool equals(Statement s);
+    int hash_code();
+
+private:
+    std::string service_name_;
+    std::string statement_string_;
+    std::map<std::string, clara::EngineData> log_and_inputs;
+    std::set<std::string> input_links;
+    std::set<std::string> output_links;
+
+    void process(std::string statement);
+    void parse_linked(std::string service_name, std::string statement);
+    bool is_log_and(std::string service_name, std::string composition);
+
 
 };
 

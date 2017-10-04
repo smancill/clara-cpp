d305a883a6a9a1449eba9ce5e8fefc1f07151571
PatrickHennis <patrickhennis@gmail.com>
2017-10-03 23:29:20 -0400

    added service state implementation

--- src/service_state.cpp
+++ src/service_state.cpp
@@ -1,6 +1,26 @@
-//
-// Created by Patrick Hennis on 9/27/17.
-//
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=8 sts=4 et sw=4 tw=80: */
+/*
+ * Copyright (c) 2016.  Jefferson Lab (JLab). All rights reserved. Permission
+ * to use, copy, modify, and distribute  this software and its documentation for
+ * educational, research, and not-for-profit purposes, without fee and without a
+ * signed licensing agreement.
+ *
+ * IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL
+ * INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ * OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS
+ * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ * PROVIDED HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * This software was developed under the United States Government license.
+ * For more information contact author at gurjyan@jlab.org
+ * Department of Experimental Nuclear Physics, Jefferson Lab.
+ */
 
 #include "service_state.hpp"
 
@@ -24,19 +44,22 @@ void ServiceState::set_state(std::string state) {
     state_ = state;
 }
 
-bool ServiceState::equals() {
-    
+bool ServiceState::equals(ServiceState ss) {
+    if (this->name_ == ss.name_ && this->state_ == ss.state_) {
+        return true;
+    }
+    return false;
 }
 
 int ServiceState::hash_code() {
-
+    int result = std::hash<std::string>{}(name_);
+    result = 31 * result + std::hash<std::string>{}(state_);
+    return result;
 }
 
 std::string ServiceState::to_string() {
     return "ServiceState{ name=\'" + name_ + ", state=\'" + state_ + "\'}";
 }
-
-
 
 }
 }

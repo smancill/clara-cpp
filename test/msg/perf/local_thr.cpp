f932f92aed7ea02374f0bd1692c0df205c2d7bc2
Sebastián Mancilla <smancill@smancill.dev>
2021-12-31 15:43:14 -0300

    Refactor performance test

--- test/msg/perf/local_thr.cpp
+++ test/msg/perf/local_thr.cpp
@@ -2,39 +2,39 @@
 #include <clara/msg/utils.hpp>
 
 #include <chrono>
-#include <condition_variable>
 #include <cstdio>
 #include <cstdlib>
+#include <exception>
+#include <future>
 #include <iostream>
-#include <mutex>
 #include <string>
-#include <vector>
 
 namespace cm = clara::msg;
 
-
-std::string bind_to;
-long message_size;
-long message_count;
-
-long elapsed_time;
-
-std::mutex mtx;
-std::condition_variable cond;
+auto elapsed_time_promise = std::promise<double>{};
 
 
 class LocalCallback
 {
 public:
+    LocalCallback(int msg_size, int msg_count)
+      : msg_size{msg_size}
+      , msg_count{msg_count}
+    { }
+
     void operator()(cm::Message& msg);
 
 private:
-    using Time = std::chrono::high_resolution_clock;
-    using point = decltype(Time::now());
+    using clock = std::chrono::high_resolution_clock;
+    using point = decltype(clock::now());
     using us = std::chrono::microseconds;
 
     void start_clock();
     long stop_clock();
+
+private:
+    const int msg_size;
+    const int msg_count;
 
     point watch;
     int nr = 0;
@@ -48,39 +48,31 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    bind_to = cm::util::to_host_addr(argv[1]);
-    message_size = std::stoi(argv[2]);
-    message_count = std::stol(argv[3]);
+    const auto bind_to = cm::util::to_host_addr(argv[1]);
+    const auto message_size = std::stoi(argv[2]);
+    const auto message_count = std::stoi(argv[3]);
 
     try {
         auto subscriber = cm::Actor("thr_subscriber");
         auto connection = subscriber.connect(cm::ProxyAddress{bind_to});
         auto topic = cm::Topic::raw("thr_topic");
-        auto cb = LocalCallback{};
+        auto cb = LocalCallback{message_size, message_count};
 
         auto sub = subscriber.subscribe(topic, std::move(connection), cb);
         std::cout << "Waiting for messages..." << std::endl;
 
-        {
-            auto lock = std::unique_lock<std::mutex>{mtx};
-            cond.wait(lock);
-        }
+        const double elapsed_time = elapsed_time_promise.get_future().get();
+        const double throughput = message_count / (elapsed_time / 1'000'000);
+        const double megabits = (throughput * message_size * 8) / 1'000'000;
+        const double latency = elapsed_time / message_count;
 
-        if (elapsed_time == 0) {
-            elapsed_time = 1;
-        }
-
-        long throughput = (long) (message_count / (double) elapsed_time * 1000000L);
-        double megabits = (double) (throughput * message_size * 8) / 1000000;
-        double latency = (double) elapsed_time / (message_count);
-        double nBits = 8;
-        printf("message elapsed: %.3f [s]\n", (double) elapsed_time / 1000000L);
-        printf("message size: %ld [B]\n", message_size);
-        printf("message count: %ld\n", message_count);
+        printf("message elapsed: %.3f [s]\n", elapsed_time / 1'000'000);
+        printf("message size: %d [B]\n", message_size);
+        printf("message count: %d\n", message_count);
         printf("mean transfer time: %.3f [us]\n", latency);
-        printf("mean transfer rate: %d [msg/s]\n", (int) throughput);
+        printf("mean transfer rate: %d [msg/s]\n", static_cast<int>(throughput));
         printf("mean throughput: %.3f [Mb/s]\n", megabits);
-        printf("mean throughput: %.3f [MByte/s]\n", megabits/nBits);
+        printf("mean throughput: %.3f [MByte/s]\n", megabits / 8);
 
         subscriber.unsubscribe(std::move(sub));
 
@@ -95,29 +87,28 @@ int main(int argc, char** argv)
 
 void LocalCallback::operator()(cm::Message& msg)
 {
-    int size = msg.data().size();
-    if (size != message_size) {
-        printf("message of incorrect size received %d\n", size);
-        exit(EXIT_FAILURE);
+    auto size = static_cast<int>(msg.data().size());
+    if (size != msg_size) {
+        std::cerr << "message of incorrect size received: " << size << std::endl;
+        std::abort();
     }
     ++nr;
     if (nr == 1) {
         start_clock();
-    } else if (nr == message_count) {
-        elapsed_time = stop_clock();
-        auto lock = std::unique_lock<std::mutex>{mtx};
-        cond.notify_one();
+    } else if (nr == msg_count) {
+        auto dt = stop_clock();
+        elapsed_time_promise.set_value(static_cast<double>(dt));
     }
 }
 
 
 void LocalCallback::start_clock()
 {
-    watch = Time::now();
+    watch = clock::now();
 }
 
 
 long LocalCallback::stop_clock()
 {
-    return std::chrono::duration_cast<us>(Time::now() - watch).count();
+    return std::chrono::duration_cast<us>(clock::now() - watch).count();
 }
